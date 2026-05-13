cf9e4fc175e5ad0f61c0a268e19511d52954da1d
aysedeveli <aysedeveli26@hacettepe.edu.tr>
2026-05-13 21:54:13 +0300

    web page update1

--- sinif-ici-alistirma/proje.R
+++ sinif-ici-alistirma/proje.R
@@ -1,7 +1,10 @@
 install.packages("dplyr")
 install.packages("readr")
+install.packages("tidyverse")
+install.packages("janitor")
 library(readr)
 library(dplyr)
+library(janitor)
 
 tum_kitaplar <- read_csv("~/GitHub/emu660-spring2026-aysedeveli/tum_kitaplar.csv")
 View(tum_kitaplar)
@@ -11,13 +14,9 @@ View(tum_kitaplar)
 kitaplar <- read_csv("tum_kitaplar.csv")
 save(kitaplar, file = "kitaplar.RData")
 
-kitaplar <- kitaplar %>%
-  mutate(
-    Fiyat = as.numeric(gsub(",", ".", Fiyat)),
-    'Yorum Sayısı' = as.numeric('Yorum Sayısı')
-  )
-
-
+kitaplar <- clearNames(kitaplar)
+names(kitaplar)
+Yayinevleri <- unique(kitaplar$Yayınevi)
 
 
 
